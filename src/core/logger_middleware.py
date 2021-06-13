ddea17b730954d8cc2d0318d420cb3f41b25e9d9
devsetgo <mikeryan56@gmail.com>
2021-06-12 20:11:34 -0400

    adding middleware

---
+++ src/core/logger_middleware.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+from loguru import logger
+from starlette.middleware.base import BaseHTTPMiddleware
+
+
+class LoggerMiddleware(BaseHTTPMiddleware):
+    async def dispatch(self, request, call_next):
+        response = await call_next(request)
+        
+        method = request.method
+        url = request.url
+        client = request.client.host
+        # print(type(url), type(client))
+
+        if "favicon.ico" not in str(url):
+            logger.info(
+                f"Request Method: {method.upper()} request via {url} accessed from {client}"
+            )
+            logger.debug(f"full_request_data: {dict(request)}")
+        return response
